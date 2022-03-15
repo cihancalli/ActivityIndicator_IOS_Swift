387a81403f19c689d294857d480d72d8a092b783
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-15 17:08:28 +0300

    UIActivityIndicatorView with standby screen showing error messages on login screen

--- ActivityIndicator/ViewController.swift
+++ ActivityIndicator/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var alertLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        alertLabel.isHidden = true
     }
-
-
+    @IBAction func didLoginButtonTapped(_ sender: Any) {
+        if usernameTextField.text == "cihan", passwordTextField.isHidden  {
+            passwordTextField.isHidden = false
+            alertLabel.isHidden = true
+            
+        } else {
+            alertLabel.isHidden = false
+            alertLabel.text = "Hata: Kullanıcı adını eksik veya yanlış girdiniz"
+            if !(passwordTextField.isHidden), passwordTextField.text == "123456"{
+                activityIndicator.startAnimating()
+                alertLabel.isHidden = true
+            } else {
+                alertLabel.isHidden = false
+                alertLabel.text = "Hata: Şifreyi eksik veya yanlış girdiniz"
+            }
+        }
+        
+    }
+    
+    
 }
 
